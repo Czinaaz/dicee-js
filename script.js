c468d17a5c4960820f857257746090443be92f72
Ivan Rudenko <rudenko.ivan81@gmail.com>
2025-03-05 15:39:23 +0100

    added animation

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var randomNumber1 = Math.random();
 randomNumber1 = randomNumber1 * 6;
 randomNumber1 = Math.floor(randomNumber1) +1;
 
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) +1;
+
 function firstPlayerDice(randomNumber1) {
     if (randomNumber1 === 1) {
         document.getElementsByClassName('img1')[0].setAttribute("src", "./images/dice1.png")
@@ -20,9 +24,7 @@ function firstPlayerDice(randomNumber1) {
 
 firstPlayerDice(randomNumber1);
 
-var randomNumber2 = Math.random();
-randomNumber2 = randomNumber2 * 6;
-randomNumber2 = Math.floor(randomNumber2) +1;
+
 
 function secondPlayerDice(randomNumber2) {
     if (randomNumber2 === 1) {
@@ -43,10 +45,28 @@ secondPlayerDice(randomNumber2);
 
 
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector('h1').innerHTML='Player 1 Won!';
-} else if (randomNumber1 < randomNumber2){
-    document.querySelector('h1').innerHTML='Player 2 Won!';
-}  else if (randomNumber1 = randomNumber2) {
-    document.querySelector('h1').innerHTML='Draw!';
-}
+
+
+
+var dice1 = document.getElementsByClassName("img1")[0];
+var dice2 = document.getElementsByClassName("img2")[0];
+
+dice1.classList.add("rolling");
+dice2.classList.add("rolling");
+
+setTimeout(function() {
+    dice1.classList.remove("rolling");
+    dice2.classList.remove("rolling");
+    firstPlayerDice(randomNumber1);
+    secondPlayerDice(randomNumber2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector('h1').innerHTML='🎉 Player 1 Won!';
+    } else if (randomNumber1 < randomNumber2){
+        document.querySelector('h1').innerHTML='🎉 Player 2 Won!';
+    }  else if (randomNumber1 = randomNumber2) {
+        document.querySelector('h1').innerHTML='Draw!';
+    }
+
+}, 1000);
+
